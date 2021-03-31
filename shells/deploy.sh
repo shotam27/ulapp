d88f9b956cdc586fbb8c50471c02fbc78ab0d72a
shotam27 <hareusagi27@gmail.com>
2021-03-31 14:46:39 +0900

    deploy.sh edited

--- shells/deploy.sh
+++ shells/deploy.sh
@@ -4,7 +4,6 @@ set -ex
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-northeast-1"
-
 export USER_NAME=${USER_NAME}
 export HOST_NAME=${HOST_NAME}
 
