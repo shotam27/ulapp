1c51338bd4221011ff0efc5b0ef10826117786dd
shotam27 <hareusagi27@gmail.com>
2021-04-01 10:26:47 +0900

    relationship#index added 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   
-  root  'events#index'
+  root 'events#index'
   
   resources :events
 
   resources :relationships, only: [:create, :destroy]
+
+  get 'relationships/index'
+  get 'relationships', to: 'relationships#index'
+
 end
