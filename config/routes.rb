b4125ff22a214e291854b01e660fc286e25d150b
shotam27 <hareusagi27@gmail.com>
2021-04-01 13:11:30 +0900

    FF button added

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   resources :users
   
   root 'events#index'
-  
   resources :events
 
   resources :relationships, only: [:create, :destroy]
-
   get 'relationships/index'
   get 'relationships', to: 'relationships#index'
 
