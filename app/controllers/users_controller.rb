f316349c979a563c7099d03ae06b72bd8efe2447
shotam27 <hareusagi27@gmail.com>
2021-04-02 23:36:16 +0900

    css.usermodel edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def show
         @user = User.find(params[:id])
+    end
+  
+    protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
     end
 end
